78858cb61dbb165a1da6023b23a78f063737d6e2
ozgurdenizyusuf <yusuftuncdemir@ogr.iu.edu.tr>
2023-09-15 09:02:01 +0300

    DerinDusunce
    DerinDusunce

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Bir soru sorun.");
+while (true)
+{
+    string soru = Console.ReadLine();
+    string cevap = "Cevap: 42";
+    Console.WriteLine(cevap);
+    Console.WriteLine("Yeni bir soru sorun.");
+}
